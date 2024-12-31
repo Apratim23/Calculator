0efdd5b67bd3293fa02dd067c0a0014d90fac365
Apratim Dutta <2205274@kiit.ac.in>
2024-12-31 13:45:28 +0530

    Initial Commit

---
+++ src/gui/CalculatorGUI.java
@@ -0,0 +1,12 @@
+package gui;
+
+import constants.CommonConstants;
+
+import javax.swing.*;
+
+public class CalculatorGUI extends JFrame {
+    public CalculatorGUI()
+    {
+        super(CommonConstants.APP_NAME);
+    }
+}
